a0303d067738041cfec98dbc53a4c86ed9f2d2fd
zzinis <lizzyin@naver.com>
2023-07-04 09:38:01 +0900

    react main

---
+++ mbti/src/pages/Join.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Wrapper, Inner } from './Main';
+
+
+
+function Join() {
+    return (
+        <>
+            <Wrapper>
+                <Inner>
+                    <h1>회원가입</h1>
+                    <p>id</p>
+                    <p>pw</p>
+                    <p>이름</p>
+                    <p>나이</p>
+                    <p>이메일 / 인증버튼</p>
+                </Inner>
+            </Wrapper>
+        </>
+    )
+}
+
+export default Join
