4335d7062dd2520c195f5073388cb68949db3720
ox01220122 <ox0122@gmail.com>
2023-07-14 13:22:49 +0900

    participation API 추가

--- controller/participation.js
+++ controller/participation.js
@@ -1,17 +1,28 @@
 const db = require('../models');
 const Participation = db.Participation;
 // 테스트 데이터 저장 API
-exports.createParticipation = (req, res) => {
+exports.createParticipation = async (req, res) => {
     const { user_id, test_name, result } = req.body;
 
-    // Participation 모델을 통해 테스트 데이터 저장
-    Participation.create({ user_id, test_name, result })
-        .then((createdParticipation) => {
-            console.log('테스트 결과 저장됨:', createdParticipation);
-            res.send({ message: '테스트 결과가 저장되었습니다.' });
-        })
-        .catch((error) => {
-            console.error('테스트 결과 저장 오류:', error);
-            res.status(500).send({ message: '테스트 결과 저장 중 오류가 발생했습니다.' });
+    try {
+        const findResult = await Participation.findAll({
+            where: {
+                user_id: user_id,
+                test_name: test_name,
+            },
         });
+
+        console.log(findResult, 'findResult');
+
+        if (!findResult || findResult.length === 0) {
+            await Participation.create({ user_id, test_name, result });
+            res.send({ message: 'update' });
+        } else {
+            await Participation.update({ result: result }, { where: { user_id: user_id, test_name: test_name } });
+            res.send({ message: 'update' });
+        }
+    } catch (error) {
+        console.error('테스트 결과 저장 오류:', error);
+        res.status(500).send({ message: '테스트 결과 저장 중 오류가 발생했습니다.' });
+    }
 };
